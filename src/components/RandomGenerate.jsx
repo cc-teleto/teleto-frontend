ad83ec6b5351b7a2d727b59d91117e05a0650a11
krkettle <catrarazou@gmail.com>
2020-11-21 14:16:07 +0900

    [update] refactoring

--- src/components/RandomGenerate.jsx
+++ src/components/RandomGenerate.jsx
@@ -1,32 +1,42 @@
 import { Button } from "@material-ui/core";
+import { useEffect } from "react";
 import { useState } from "react";
+
+
+// 表示内容をfetchURLから取得し、stateを更新する
+const fetchContent = async (fetchURL, setContent) => {
+  try {
+    const res = await fetch(fetchURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    setContent(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default function RandomGenerate(props) {
   const [content, setContent] = useState("");
+  useEffect(() => {
+    fetchContent(props.fetchURL, setContent);
+  }, [props.fetchURL]);
 
   const onClick = async () => {
-    (async () => {
-      const res = await fetch(props.fetchURL, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }).catch((err) => {
-        console.log(err);
-      });
-      const data = await res.json().catch((err) => {
-        console.log(err);
-      });
-      setContent(data);
-    })();
+    fetchContent(content, props.fetchURL);
   };
 
   return (
     <div>
       <p>{props.title}</p>
       <p>{content}</p>
-      <Button variant="contained" onClick={onClick}>{props.buttonTitle}</Button>
+      <Button variant="contained" onClick={onClick}>
+        {props.buttonTitle}
+      </Button>
     </div>
   );
 }
