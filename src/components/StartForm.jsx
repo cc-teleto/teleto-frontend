0df637513ea1b5af8dc733fb8f82e33d6569a559
krkettle <catrarazou@gmail.com>
2020-11-21 14:21:19 +0900

    [update] refactoring

--- src/components/StartForm.jsx
+++ src/components/StartForm.jsx
@@ -8,16 +8,35 @@ import StartFormInput from "./StartFormInput";
 import _ from "lodash";
 
 const MEMBER_POST_URL = getURL("/members");
+const MAX_PERIOD = 3;
+const PERIOD_INTERVAL = 0.5;
 
+// 開催時間の選択リストを作成する
 const periodSelectList = [];
-for (let hour of _.range(0, 5, 0.5)) {
-  hour = hour + 0.5;
+for (let hour of _.range(0, MAX_PERIOD, PERIOD_INTERVAL)) {
+  hour = hour + PERIOD_INTERVAL;
   periodSelectList.push(
     <MenuItem value={hour} key={hour}>
       {hour}時間
     </MenuItem>
   );
 }
+
+// 参加者のリストをAPIに送信する
+const postMemberNames = async (memberNames) => {
+  const body = memberNames.trim();
+  await fetch(MEMBER_POST_URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(body),
+  }).catch((err) => {
+    console.log(err);
+  });
+};
 
 export default function StartForm(props) {
   const [memberNames, setMemberNames] = useState(props.memberNames);
@@ -30,25 +49,12 @@ export default function StartForm(props) {
   const memberNamesField = {
     id: "memberNames",
     name: "参加者一覧(カンマ区切り)",
-    state: memberNames
-  }
+    state: memberNames,
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    (async () => {
-      const body = memberNames.trim();
-      await fetch(MEMBER_POST_URL, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        mode: "cors",
-        body: JSON.stringify(body),
-      }).catch((err) => {
-        console.log(err);
-      });
-    })();
+    const res = postMemberNames(memberNames);
     setCurrentView(CURRENT_VIEW.RANDOM_GENERATE);
   };
 
@@ -61,7 +67,9 @@ export default function StartForm(props) {
           selectList={periodSelectList}
         />
         <StartFormInput field={memberNamesField} dispatch={setMemberNames} />
-        <Button variant="contained" type="submit">開始</Button>
+        <Button variant="contained" type="submit">
+          開始
+        </Button>
       </form>
     </main>
   );
