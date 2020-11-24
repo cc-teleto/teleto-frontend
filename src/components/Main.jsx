cf325271db0f7df2953fcc374a3e6e6021930837
湯浅海貴 <yuasa.kshu@gmail.com>
2020-11-24 18:59:35 +0900

    [add] memberList component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { CURRENT_VIEW, getURL } from "../const";
 import AppContext from "../context/AppContext";
 import RandomGenerate from "./RandomGenerate";
+import MembersList from "./MembersList";
 import StartForm from "./StartForm";
 
 
@@ -10,10 +11,12 @@ export default function Main() {
   const { groupHash, currentView } = useContext(AppContext);
   const [topicFetchURL, setTopicFetchURL] = useState();
   const [memberFetchURL, setMemberFetchURL] = useState();
+  const [allMemberFetchURL, setAllMemberFetchURL] = useState();
 
   useEffect( () => {
     setTopicFetchURL(getURL("/topics", `/?random=true&grouphash=${groupHash}`))
     setMemberFetchURL(getURL("/members", `/?random=true&grouphash=${groupHash}`))
+    setAllMemberFetchURL(getURL("/members", `/?random=false&grouphash=${groupHash}`))
   }, [groupHash])
 
   if (currentView === CURRENT_VIEW.START_FORM) {
@@ -30,6 +33,11 @@ export default function Main() {
           title="話者"
           buttonTitle="話者切替"
           fetchURL={memberFetchURL}
+        />
+        <MembersList
+          title="話者"
+          buttonTitle="話者切替"
+          fetchURL={allMemberFetchURL}
         />
       </>
     )
