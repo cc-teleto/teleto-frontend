2a54fd9b3dd73464111b725537a35ee14987bdf0
krkettle <catrarazou@gmail.com>
2020-11-24 15:58:35 +0900

    [update] Logo.jsx follow airbnb eslint

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,21 @@
-import logoImage from "../img/logo.png"
-import { CURRENT_VIEW } from "../const"
-import { useContext } from "react";
+import React, { useContext } from "react";
+import logoImage from "../img/logo.png";
+import { CURRENT_VIEW } from "../const";
 import AppContext from "../context/AppContext";
 
 export default function Logo() {
   const { setCurrentView } = useContext(AppContext);
   const onClick = () => {
-    setCurrentView(CURRENT_VIEW.START_FORM)
-  }
+    setCurrentView(CURRENT_VIEW.START_FORM);
+  };
   return (
-    <img className="logo" src={logoImage} alt="logo" onClick={onClick} />
-  )
+    <input
+      type="image"
+      className="logo"
+      src={logoImage}
+      alt="logo"
+      onClick={onClick}
+      onKeyDown={onClick}
+    />
+  );
 }
