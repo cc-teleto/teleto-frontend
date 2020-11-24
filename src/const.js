717d6b4422fd307803ee7350a6808aa85c15f265
krkettle <catrarazou@gmail.com>
2020-11-24 22:27:28 +0900

    [update] const.js follow airbnb eslint

--- src/const.js
+++ src/const.js
@@ -1,12 +1,13 @@
 import path from "path";
 
-export const  CURRENT_VIEW = {
+export const CURRENT_VIEW = {
   START_FORM: "START_FORM",
-  RANDOM_GENERATE: "RANDOM_GENERATE"
-}
+  RANDOM_GENERATE: "RANDOM_GENERATE",
+};
 
-export const API_URL = "https://ttnxcty7yc.execute-api.us-east-2.amazonaws.com/";
+export const API_URL =
+  "https://ttnxcty7yc.execute-api.us-east-2.amazonaws.com/";
 export const STAGE = "stage";
 export const getURL = (...args) => {
-  return path.join(API_URL, STAGE, ...args)
-}
+  return path.join(API_URL, STAGE, ...args);
+};
