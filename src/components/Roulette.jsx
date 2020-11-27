1f1145693d9f9a4fd50ed29fa4b900282b7e146a
tanabe <m.k.masakatsu@gmail.com>
2020-11-27 21:55:17 +0900

    haikei 、roulette回ります。

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Box } from "@material-ui/core";
 import Winwheel from "../utils/Winwheel";
 import AppContext from "../context/AppContext";
+import "../styles/styles.css";
 
 export default function Roulette() {
   const { members } = useContext(AppContext);
-  let theWheel;
+  const [wheel, setWheel] = useState();
   const colorList = ["#eae56f", "#89f26e", "#7de6ef", "#e7706f"];
 
   // Called when the animation has finished.
@@ -23,36 +24,26 @@ export default function Roulette() {
       return { fillStyle: colorList[index % 4], text: value };
     });
 
-    theWheel = new Winwheel({
-      canvasId: "myCanvas",
-      numSegments: members.maxId, // Number of segments
-      outerRadius: 100, // The size of the wheel.
-      // 'centerX'      : 217,       // Used to position on the background correctly.
-      // 'centerY'      : 219,
-      textFontSize: 10, // Font size.
-      // Definition of all the segments.
-
-      segments: segmentList,
-      //   [
-      //     { fillStyle: "#eae56f", text: "Prize 1" },
-      //     { fillStyle: "#89f26e", text: "Prize 2" },
-      //     { fillStyle: "#7de6ef", text: "Prize 3" },
-      //     { fillStyle: "#e7706f", text: "Prize 4" },
-      //     { fillStyle: "#eae56f", text: "Prize 5" },
-      //     { fillStyle: "#89f26e", text: "Prize 6" },
-      //     { fillStyle: "#7de6ef", text: "Prize 7" },
-      //     { fillStyle: "#e7706f", text: "Prize 8" },
-      //   ],
-      // Definition of the animation
-      animation: {
-        type: "spinToStop",
-        duration: 5,
-        spins: 8,
-        callbackFinished: alertPrize,
-      },
-    });
-    console.log(theWheel);
-  }, []);
+    setWheel(
+      new Winwheel({
+        canvasId: "myCanvas",
+        numSegments: Object.values(members.members).length, // Number of segments
+        outerRadius: 110, // The size of the wheel.
+        centerX: 217, // Used to position on the background correctly.
+        centerY: 222,
+        textFontSize: 10, // Font size.
+        // Definition of all the segments.
+        segments: segmentList,
+        animation: {
+          type: "spinToStop",
+          duration: 5,
+          spins: 8,
+          callbackFinished: alertPrize,
+        },
+      })
+    );
+    console.log(wheel);
+  }, [members.maxId]);
 
   // Vars used by the code in this page to do power controls.
   const wheelPower = 0;
@@ -63,15 +54,17 @@ export default function Roulette() {
   // -------------------------------------------------------
   function startSpin() {
     // Ensure that spinning can't be clicked again while already running.
+    console.log(wheelSpinning);
     if (wheelSpinning === false) {
       // Based on the power level selected adjust the number of spins for the wheel, the more times is has
       // to rotate with the duration of the animation the quicker the wheel spins.
+      console.log(wheel);
       if (wheelPower === 1) {
-        theWheel.animation.spins = 3;
+        wheel.animation.spins = 3;
       } else if (wheelPower === 2) {
-        theWheel.animation.spins = 8;
+        wheel.animation.spins = 8;
       } else if (wheelPower === 3) {
-        theWheel.animation.spins = 15;
+        wheel.animation.spins = 15;
       }
 
       // Disable the spin button so can't click again while wheel is spinning.
@@ -79,7 +72,7 @@ export default function Roulette() {
       document.getElementById("spin_button").className = "";
 
       // Begin the spin animation by calling startAnimation on the wheel object.
-      theWheel.startAnimation();
+      wheel.startAnimation();
 
       // Set to true so that power can't be changed and spin button re-enabled during
       // the current animation. The user will have to reset before spinning again.
@@ -89,9 +82,11 @@ export default function Roulette() {
 
   return (
     <Box justifyContent="center" display="flex">
-      <canvas id="myCanvas" width="250" height="250">
-        {" "}
-      </canvas>
+      <div className="canvas" width="438" height="582">
+        <canvas id="myCanvas" width="434" height="434">
+          {" "}
+        </canvas>
+      </div>
       <input
         type="button"
         value="SPIN"
