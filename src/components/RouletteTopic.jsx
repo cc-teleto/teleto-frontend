5a013b1c3d1b51947cca8ee1e7f21fd5741352a8
sabarasaraba <sabarasaraba@users.noreply.github.com>
2020-11-29 22:54:46 +0900

    [add] トピックルーレットの回転と同期完了 Co-authored-by: sabarasaraba <sabarasaraba@users.noreply.github.com> Co-authored-by: ishuuuu <ishuuuu@users.noreply.github.com>

--- src/components/RouletteTopic.jsx
+++ src/components/RouletteTopic.jsx
@@ -23,11 +23,49 @@ export default function RouletteTopic() {
   const [wheel, setWheel] = useState();
   const [wheelSpinning, setWheelSpinning] = useState(false);
   const [wheelStopped, setWheelStopped] = useState(false);
+  const [topics, setTopics] = useState([]);
   const colorList = ["#eae56f", "#89f26e", "#7de6ef", "#e7706f"];
   // const [topics, setTopics] = useState([]);
   const audio = new Audio("/tick.mp3");
   let theme = createMuiTheme();
   theme = responsiveFontSizes(theme);
+
+  // const loadingWheel = new Winwheel({
+  //   canvasId: "loadingRoulette",
+  //   numSegments: 4, // Number of segments
+  //   pointerAngle: 135, // Ensure this is set correctly
+  //   outerRadius: 165, // The size of the wheel.
+  //   innerRadius: 50,
+  //   centerX: 217, // Used to position on the background correctly.
+  //   centerY: 222,
+  //   strokeStyle: "#ffffff",
+  //   lineWidth: 1,
+  //   // textOrientation: "vertical",
+  //   textFontSize: 18, // Font size.\
+  //   rotationAngle: -360 / 4 / 2, // show the default position aligned to the text
+  //   // Definition of all the segments.
+  //   segments: [
+  //     { fillStyle: "#eae56f", text: "Segment 1" },
+  //     { fillStyle: "#89f26e", text: "Segment 2" },
+  //     { fillStyle: "#7de6ef", text: "Segment 3" },
+  //     { fillStyle: "#e7706f", text: "Segment 4" },
+  //   ],
+  //   // Specify pin parameters.
+  //   pins: {
+  //     number: 4,
+  //     outerRadius: 6,
+  //     margin: 3,
+  //     fillStyle: "#47B7C1",
+  //     strokeStyle: "#47B7C1",
+  //   },
+  //   animation: {
+  //     type: "spinOngoing",
+  //     duration: 5,
+  //     spins: 8,
+  //   },
+  // });
+
+  // console.log(loadingWheel);
 
   // This function is called when the sound is to be played.
   function playSound() {
@@ -76,7 +114,7 @@ export default function RouletteTopic() {
       // the current animation. The user will have to reset before spinning again.
       setWheelSpinning(false);
     }
-  }, [wheelSpinning]);
+  }, [wheelSpinning, wheel]);
 
   // ルーレット生成のためのトピックを取得し、ルーレット生成
   useEffect(() => {
@@ -90,7 +128,46 @@ export default function RouletteTopic() {
   }, []);
 
   useEffect(() => {
+    const itemNumber = topics.length;
+    setWheel(
+      new Winwheel({
+        canvasId: "topicRoulette",
+        numSegments: itemNumber, // Number of segments
+        pointerAngle: 135, // Ensure this is set correctly
+        outerRadius: 165, // The size of the wheel.
+        innerRadius: 50,
+        centerX: 217, // Used to position on the background correctly.
+        centerY: 222,
+        strokeStyle: "#ffffff",
+        lineWidth: 1,
+        // textOrientation: "vertical",
+        textFontSize: 18, // Font size.\
+        rotationAngle: -360 / itemNumber / 2, // show the default position aligned to the text
+        // Definition of all the segments.
+        segments: topics,
+        // Specify pin parameters.
+        pins: {
+          number: itemNumber,
+          outerRadius: 6,
+          margin: 3,
+          fillStyle: "#47B7C1",
+          strokeStyle: "#47B7C1",
+        },
+        animation: {
+          type: "spinToStop",
+          duration: 5,
+          spins: 8,
+          callbackFinished: stopAction,
+          callbackSound: playSound, // Function to call when the tick sound is to be triggered.
+          soundTrigger: "pin",
+        },
+      })
+    );
+  }, [topics.length, topics]);
+
+  useEffect(() => {
     console.log("=====ws-useEffect Start=====");
+    console.log(wheel);
     if (!ws) return;
     ws.onmessage = (e) => {
       console.log("Topic:receiveData", e.data);
@@ -99,13 +176,12 @@ export default function RouletteTopic() {
       // バックエンドバグ修正までコメントアウト
       if (resData.action === "getmultitopics") {
         console.log("*****getmultitopics Start*****");
-        const segmentList = Object.values(resData.topics).map(function (
-          value,
-          index
-        ) {
+
+        const segmentList = resData.topics.map(function (value, index) {
+          console.log(value.topic.replace(/(?:[\w\s]{16})/g, "$&|\n"));
           return {
             fillStyle: colorList[index % colorList.length],
-            text: value,
+            text: value.keyword.replace(/(?:[\w\s]{16})/g, "$&|\n"),
           };
         });
         // const segmentList = [
@@ -117,42 +193,7 @@ export default function RouletteTopic() {
         console.log("*****segmentList*****");
         console.log(segmentList);
 
-        const itemNumber = segmentList.length;
-
-        setWheel(
-          new Winwheel({
-            canvasId: "topicRoulette",
-            numSegments: itemNumber, // Number of segments
-            pointerAngle: 135, // Ensure this is set correctly
-            outerRadius: 165, // The size of the wheel.
-            innerRadius: 50,
-            centerX: 217, // Used to position on the background correctly.
-            centerY: 222,
-            strokeStyle: "#ffffff",
-            lineWidth: 1,
-            // textOrientation: "vertical",
-            textFontSize: 18, // Font size.\
-            rotationAngle: -360 / itemNumber / 2, // show the default position aligned to the text
-            // Definition of all the segments.
-            segments: segmentList,
-            // Specify pin parameters.
-            pins: {
-              number: itemNumber,
-              outerRadius: 6,
-              margin: 3,
-              fillStyle: "#47B7C1",
-              strokeStyle: "#47B7C1",
-            },
-            animation: {
-              type: "spinToStop",
-              duration: 5,
-              spins: 8,
-              callbackFinished: stopAction,
-              callbackSound: playSound, // Function to call when the tick sound is to be triggered.
-              soundTrigger: "pin",
-            },
-          })
-        );
+        setTopics(segmentList);
       }
 
       if (resData.action === "startroulette") {
@@ -162,7 +203,7 @@ export default function RouletteTopic() {
         }
       }
     };
-  }, [ws]);
+  }, [ws, wheel]);
 
   function handleOnClick() {
     const data = {
@@ -181,20 +222,42 @@ export default function RouletteTopic() {
         </Typography>
       </ThemeProvider>
       {/* set className to show the background image */}
-      <div className="canvas_logo" width="438" height="582">
-        <canvas id="topicRoulette" width="434" height="434">
-          {" "}
-        </canvas>
-      </div>
-      <Button
-        variant="contained"
-        onClick={() => handleOnClick()}
-        style={{
-          backgroundColor: "#9fe4e2",
-        }}
-      >
-        START
-      </Button>
+      {/* {wheel ?  */}
+      <>
+        <div className="canvas_logo" width="438" height="582">
+          <canvas id="topicRoulette" width="434" height="434">
+            {" "}
+          </canvas>
+        </div>
+        <Button
+          variant="contained"
+          onClick={() => handleOnClick()}
+          style={{
+            backgroundColor: "#9fe4e2",
+          }}
+        >
+          START
+        </Button>
+      </>
+      {/* //  : 
+      //   <>
+      //     <div className="canvas_logo" width="438" height="582">
+      //       <canvas id="loadingRoulette" width="434" height="434">
+      //         {" "}
+      //       </canvas>
+      //     </div>
+      //     <Button
+      //       variant="contained"
+      //       style={{
+      //         backgroundColor: "#9fe4e2",
+      //       }}
+      //       disabled
+      //     >
+      //       START
+      //     </Button>
+      //   </>
+      // }
+      // ) */}
     </>
   );
 }
