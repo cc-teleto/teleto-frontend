d469e1b5db4889b024ca99e967ace30119a2f297
Shu Ishikura <i.shuuuu0723@gmail.com>
2020-12-02 14:43:19 +0900

    URLCOPYの描画を調整

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 // import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, Grid } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 import moment from "moment";
 
@@ -69,28 +69,34 @@ export default function Message(props) {
 
   return (
     <Box display="flex" flexWrap="nowrap" width="90%">
-      <Box width="50%">{alertMsg}</Box>
-      <Box display="flex" flexWrap="nowrap" width="100%">
-        <font face="arial">
-          <Box flexShrink={0}>
-            {timeLeft > 0 ? `残り時間: ${timeLeft}分` : ""}
+      <Box width="0%">{alertMsg}</Box>
+      <Grid container spacing={0}>
+        <Grid item xs={0} sm={3} ><></></Grid>
+        <Grid item xs={8} sm={5}>
+          <Box display="flex" flexWrap="nowrap" width="100%">
+            <font face="arial">
+              <Box flexShrink={0}>
+                {timeLeft > 0 ? `残り時間: ${timeLeft}分` : ""}
+              </Box>
+            </font>
           </Box>
-        </font>
-      </Box>
-      <Box display="flex" flexWrap="nowrap" width="100%" align-items="center">
-        <Button
-          variant="contained"
-          onClick={() => urlCopy()}
-          style={{
-            backgroundColor: "#9fe4e2",
-            width: "100px",
-            height: "20px",
-            margin: "2px 20px",
-          }}
-        >
-          URLCOPY
-        </Button>
-      </Box>
+        </Grid>
+        <Grid item xs={4} sm={1}>
+          <Box display="flex" flexWrap="nowrap" width="100%" align-items="flex-end">
+            <Button
+              align-items="flex-end"
+              variant="contained"
+              onClick={() => urlCopy()}
+              style={{
+                backgroundColor: "#9fe4e2",
+              }}
+            >
+              URLCOPY
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={0} sm={3} ><></></Grid>
+      </Grid>
     </Box>
   );
 }
